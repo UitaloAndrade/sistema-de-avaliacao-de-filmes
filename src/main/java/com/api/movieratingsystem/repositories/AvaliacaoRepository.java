8a1ae13f36636f424abe5ea412db2292491d05cb
UitaloAndrade <uitalo.a.reis@gmail.com>
2023-08-06 22:27:27 -0300

    Criando Query personalizada

--- src/main/java/com/api/movieratingsystem/repositories/AvaliacaoRepository.java
+++ src/main/java/com/api/movieratingsystem/repositories/AvaliacaoRepository.java
@@ -3,8 +3,12 @@ package com.api.movieratingsystem.repositories;
 import com.api.movieratingsystem.models.AvaliacaoModel;
 import com.api.movieratingsystem.models.FilmeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AvaliacaoRepository extends JpaRepository<AvaliacaoModel, Long> {
+    List<AvaliacaoModel> findByFilme(FilmeModel filmeModel);
 }
