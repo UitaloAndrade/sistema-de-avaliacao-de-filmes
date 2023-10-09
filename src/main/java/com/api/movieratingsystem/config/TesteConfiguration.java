768c1f11c0e3e4a1e3294e49edfef766e626189c
UitaloAndrade <uitalo.a.reis@gmail.com>
2023-10-09 16:21:06 -0300

    Refatorando config profile 'test'

--- src/main/java/com/api/movieratingsystem/config/TesteConfiguration.java
+++ src/main/java/com/api/movieratingsystem/config/TesteConfiguration.java
@@ -6,7 +6,7 @@ import com.api.movieratingsystem.models.Usuario;
 import com.api.movieratingsystem.repositories.FilmeRepository;
 import com.api.movieratingsystem.repositories.UsuarioRepository;
 import com.api.movieratingsystem.services.AvaliacaoService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,33 +15,32 @@ import java.util.Arrays;
 
 @Configuration
 @Profile("test")
+@AllArgsConstructor
 public class TesteConfiguration implements CommandLineRunner {
 
-    @Autowired
     private FilmeRepository filmeRepository;
-    @Autowired
     private AvaliacaoService avaliacaoService;
-    @Autowired
     private UsuarioRepository usuarioRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
-        Filme filme1 = new Filme("Harry Potter", "Uitalo", "2001/01/01", "Um filme epico da minha infancia");
-        Filme filme2 = new Filme("Velozes e furiosos", "Uitalo", "2001/01/01", "Melhor filme sobre carros");
+        Filme filme1 = new Filme("Filme 01", "Diretor 01", "2001/01/01", "Sinopse 01");
+        Filme filme2 = new Filme("Filme 02", "Diretor 02", "2001/01/01", "Sinopse 02");
 
         filmeRepository.saveAll(Arrays.asList(filme1, filme2));
 
-        Usuario usuario1 = new Usuario("Fernando");
-        Usuario usuario2 = new Usuario("Pedro");
-        Usuario usuario3 = new Usuario("Joao");
+        Usuario usuario1 = new Usuario("Usuario 01");
+        Usuario usuario2 = new Usuario("Usuario 02");
+        Usuario usuario3 = new Usuario("Usuario 03");
 
         usuarioRepository.saveAll(Arrays.asList(usuario1, usuario2, usuario3));
 
-        Avaliacao avaliacao1 = new Avaliacao(filme1, 8, "um filme muito bom = av1", usuario1);
-        Avaliacao avaliacao2 = new Avaliacao(filme1, 9, "um filme muito bom = av2", usuario1);
-        Avaliacao avaliacao3 = new Avaliacao( filme2, 8, "um filme mais ou menos = av3", usuario2);
-        Avaliacao avaliacao4 = new Avaliacao(filme2, 10, "um filme muito bom = av4", usuario2);
-        Avaliacao avaliacao5 = new Avaliacao(filme1, 5, "um filme muito bom = av5", usuario3);
+        Avaliacao avaliacao1 = new Avaliacao(filme1, 5, "Avaliacao 01", usuario1);
+        Avaliacao avaliacao2 = new Avaliacao(filme1, 5, "Avaliacao 02", usuario1);
+        Avaliacao avaliacao3 = new Avaliacao( filme2, 5, "Avaliacao 03", usuario2);
+        Avaliacao avaliacao4 = new Avaliacao(filme2, 5, "Avaliacao 04", usuario2);
+        Avaliacao avaliacao5 = new Avaliacao(filme1, 5, "Avaliacao 05", usuario3);
 
         avaliacaoService.salvar(avaliacao1);
         avaliacaoService.salvar(avaliacao2);
